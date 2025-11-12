8769d5ededc0662e9dffd936e7a68726adeda3fe
Mansour <Mansour@thecreativenerds.ie>
2025-11-12 15:58:46 +0000

    Add file envy.py to gitgnore

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k%pv@os(zoci!#ve$%=_wxf#s$d1wli$+4cn6bx8eprremqyi9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
